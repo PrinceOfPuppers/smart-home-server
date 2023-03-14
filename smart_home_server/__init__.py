530a06a381a288f8883676eecded61413800123d
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2023-03-13 23:53:14 -0400

    auto update test

--- smart_home_server/__init__.py
+++ smart_home_server/__init__.py
@@ -1,7 +1,7 @@
 from threading import current_thread
 from signal import signal, SIGINT, Signals#,SIGABRT,SIGTERM
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 class InterruptTriggered(Exception):
     pass
